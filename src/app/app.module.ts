5befe8edcfa262ca3f98f3b83b87f5b48ce86d77
TaisBritoCosta <104440707+TaisBritoCosta@users.noreply.github.com>
2022-07-04 23:59:27 -0300

    Consumo de API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +21,10 @@ import { CadastrarComponent } from './cadastrar/cadastrar.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/cadastrar/cadastrar.component.ts
+++ src/app/cadastrar/cadastrar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-cadastrar',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CadastrarComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario()
+  confirmSenha: string
+  tipoUsuario: string
+  constructor(private authService: AuthService,private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+    window.scroll(0,0) 
+  }
+  confirmarSenha(event:any){
+      this.confirmSenha = event.target.value
+  }
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
   }
 
+  cadastrar() {
+    this.usuario.tipo = this.tipoUsuario;
+
+    if (this.usuario.senha != this.confirmSenha) {
+      alert('Senhas diferentes!');
+    } else {
+      this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+        this.usuario = resp;
+        this.router.navigate(['/entrar']);
+        alert('Usuário cadastrado com sucesso!');
+      });
+    }
+  }
 }
