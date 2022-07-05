15dabcd453eb5a2968a8a3a297883d6e3eac72f6
TaisBritoCosta <104440707+TaisBritoCosta@users.noreply.github.com>
2022-07-05 00:50:41 -0300

    Adicionando rotas

--- src/app/entrar/entrar.component.ts
+++ src/app/entrar/entrar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-entrar',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entrar.component.css']
 })
 export class EntrarComponent implements OnInit {
+  usuarioLogin: UsuarioLogin = new UsuarioLogin();
 
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    window.scroll(0, 0);
   }
 
+  entrar() {
+    this.authService.entrar(this.usuarioLogin).subscribe((resp: UsuarioLogin)=> {
+        this.usuarioLogin = resp;
+
+        environment.token = this.usuarioLogin.token;
+        environment.nome = this.usuarioLogin.nome;
+        environment.foto = this.usuarioLogin.foto;
+        environment.id = this.usuarioLogin.id;
+
+        console.log(environment.token);
+        console.log(environment.nome);
+        console.log(environment.foto);
+        console.log(environment.id);
+
+        this.router.navigate(['/inicio']);
+      },erro => {
+        if (erro.status == 500) {
+          alert('Usuário ou senha estão incorretos!');
+        }
+      }
+    );
+  }
 }
+
